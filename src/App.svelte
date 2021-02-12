9fc9ee9669fc112bc69ae5cdd72e875e08324a92
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2021-02-12 22:32:37 +0530

    create poll form completed

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,30 @@
 <script>
-	export let name;
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+  import CreatePollForm from "./components/CreatePollForm.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+
+  // tabs
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+  const tabChange = (e) => (activeItem = e.detail);
 </script>
 
+<Header />
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Hallo! Ich bin svelte</p>
+  <Tabs {activeItem} {items} on:tabChange={tabChange} />
+  {#if activeItem === "Current Polls"}
+    <p>poll list goes here</p>
+  {:else if activeItem === "Add New Poll"}
+    <CreatePollForm />
+  {/if}
 </main>
+<Footer />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    width: 100%;
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
