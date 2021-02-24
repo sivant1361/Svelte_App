a49df589e39a4a407c193bcd2652c9b6cf30ea90
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2021-02-24 23:39:17 +0530

    vote counter

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,5 +1,7 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import Button from "../shared/Button.svelte";
+  let dispatch = createEventDispatcher();
   let fields = { question: "", answerA: "", answerB: "" };
   let errors = { question: "", answerA: "", answerB: "" };
   let valid = false;
@@ -28,7 +30,8 @@
     }
     // add new poll
     if (valid) {
-      console.log("valid", fields);
+      let poll = { ...fields, id: Math.random(), votesA: 0, votesB: 0 };
+      dispatch("add", poll);
     }
   };
 </script>
---
+++ src/components/PollDetails.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Card from '../shared/Card.svelte';
+  import { createEventDispatcher } from 'svelte';
+  export let poll;
+  const dispatch = createEventDispatcher();
+  // reactive values
+  $: totalVotes = poll.votesA + poll.votesB;
+  // handling votes
+  const handleVote = (option, id) => {
+    dispatch('vote', {option, id});
+  };
+</script>
+
+<Card>
+  <div class="poll">
+    <h3>{ poll.question }</h3>
+    <p>Total votes: { totalVotes }</p>
+    <div class="answer" on:click={() => handleVote('a', poll.id)}>
+      <div class="percent percent-a"></div>
+      <span>{ poll.answerA } ({ poll.votesA } votes)</span>
+    </div>
+    <div class="answer" on:click={() => handleVote('b', poll.id)}>
+      <div class="percent percent-b"></div>
+      <span>{ poll.answerB } ({ poll.votesB } votes)</span>
+    </div>
+  </div>
+</Card>
+
+<style>
+  h3{
+    margin: 0 auto;
+    color: #555;
+  }
+  p{
+    margin-top: 6px;
+    font-size: 14px;
+    color: #aaa;
+    margin-bottom: 30px;
+  }
+  .answer{
+    background: #fafafa;
+    cursor: pointer;
+    margin: 10px auto;
+    position: relative;
+  }
+  .answer:hover{
+    opacity: 0.6;
+  }
+  span{
+    display: inline-block;
+    padding: 10px 20px;
+  }
+</style>
